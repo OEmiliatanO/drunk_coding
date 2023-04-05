70a39305c20c574e4c0490c21a83770cbbe57486
OEmiliatanO <emiliaistruelove@gmail.com>
2023-04-05 12:37:47 +0800

    exclude .elf

---
+++ unordered_reading/README.md
@@ -0,0 +1,18 @@
+testcase 1~2:
+
+n=100, mini=0, maxi=100
+
+
+testcase 3~5:
+
+n=10,000, mini=0, maxi=100
+
+
+testcase 6~7:
+
+n=100,000,000, mini=0, maxi=1
+
+
+testcase 8~10:
+
+n=100,000,000, mini=-10,000, maxi=10,000
---
+++ unordered_reading/solution/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+constexpr int MAXN = 100000000;
+
+std::vector<int> arr;
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	std::size_t n;
+	std::cin >> n;
+	arr.resize(n);
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> arr[i];
+	}
+
+	std::sort(arr.begin(), arr.end());
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << arr[i] << " \n"[i==n-1];
+	}
+}
