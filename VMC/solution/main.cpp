d0f01e489a8b6af88ddfd1f543fae549480e2f77
OEmiliatanO <emiliaistruelove@gmail.com>
2023-04-17 20:51:35 +0800

    update solution

--- VMC/solution/main.cpp
+++ VMC/solution/main.cpp
@@ -6,10 +6,10 @@ std::vector<long long> arr;
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-    int t;
-    std::cin >> t;
+	//std::ios_base::sync_with_stdio(false);
+	//std::cin.tie(nullptr);
+	std::size_t t;
+	scanf("%lu", &t);
     while (t--)
     {
         std::size_t n;
@@ -18,7 +18,7 @@ int main()
         long long s = 0;
         for (std::size_t i = 0; i < n; ++i)
         {
-            std::cin >> arr[i];
+			scanf("%lld", &arr[i]);
             s += arr[i];
         }
         double mid = (double)s/n;
@@ -27,6 +27,7 @@ int main()
         {
             ans += std::abs(arr[i]-mid);
         }
-        std::cout << mid << " " << ans << '\n';
+        //std::cout << mid << " " << ans << '\n';
+		printf("%.5f %.5f\n", mid, ans);
     }
 }
